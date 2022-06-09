3a08bb583c7e3744c72533248786b13643dfaf86
Chang Shin <cshin7374@gmail.com>
2022-06-09 00:49:18 -0500

    commit tree 9549e4d69e8a9de96fc084e3e6d2e0a00fc5882a

--- src/index.js
+++ src/index.js
@@ -12,4 +12,78 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //fetch
+  fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(toys => { toys.forEach(toy => renderToy(toy)) })
+
+  //callback renderToy
+  function renderToy(toy) {
+      const div = document.createElement('div');
+      div.className = "card";
+      const h2 = document.createElement('h2');
+      const img = document.createElement('img');
+      img.className = 'toy-avatar';
+      const p = document.createElement('p');
+      const bttn = document.createElement('button');
+      bttn.className = 'like-btn';
+      bttn.id = `${toy.id}`
+      
+      h2.textContent = toy.name;
+      img.src = toy.image;
+      p.textContent = `${toy.likes} Likes`;
+      bttn.textContent = 'Like ❤️'
+
+      bttn.addEventListener('click', () => {
+        toy.likes += 1;
+        p.textContent = `${toy.likes} Likes`;
+        patchToy(toy);
+      })
+      
+      const collection = document.querySelector('#toy-collection')
+      div.append(h2, img, p, bttn);
+      collection.append(div);
+  }
+  //submit event listener 
+  const form = document.querySelector('.add-toy-form');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const newToy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    renderToy(newToy);
+    postToy(newToy);
+  });
+
+  //post
+  function postToy(toy) {
+    fetch("http://localhost:3000/toys", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toy)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+  //patch
+  function patchToy(updatedToy) {
+    fetch(`http://localhost:3000/toys/${updatedToy.id}`, {
+      method: 'PATCH',
+      headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(updatedToy) })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  
+  }
+ 
 });
